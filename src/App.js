2f901648e2a78b7f1fa90022feb0ce2bd6715bfb
akifsonmez <akifsonmez@outlook.com>
2022-07-12 11:05:56 +0300

    Add fetch example with react query

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import Home from "./pages/Home";
 import Pokemon from "./pages/Pokemon";
 import RQPokemon from "./pages/RQPokemon";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
-        <div>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/pokemon">Pokemon</Link>
-            <Link to="/rqpokemon">RQ Pokemon</Link>
-          </nav>
-        </div>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/pokemon" element={<Pokemon />}></Route>
-          <Route path="/rqpokemon" element={<RQPokemon />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <div>
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/pokemon">Pokemon</Link>
+              <Link to="/rqpokemon">RQ Pokemon</Link>
+            </nav>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/pokemon" element={<Pokemon />}></Route>
+            <Route path="/rqpokemon" element={<RQPokemon />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/RQPokemon.js
+++ src/pages/RQPokemon.js
@@ -1,7 +1,25 @@
+import { useQuery } from "react-query";
+
+const fetchPokemon = () => {
+  return fetch("http://localhost:4000/pokemon").then((resp) => resp.json());
+};
+
 export default function RQFunction() {
+  const { data, error, isLoading } = useQuery("pokemonList", fetchPokemon);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error...</div>;
+  }
+
   return (
     <div>
       <h1>Pokemon List (with React Query)</h1>
+      {data?.map((p) => (
+        <p key={p.name}>{p.name}</p>
+      ))}
     </div>
   );
 }
