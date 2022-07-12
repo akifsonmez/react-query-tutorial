c827a4f7105907a122fc915187857a18aa6a64f3
akifsonmez <akifsonmez@outlook.com>
2022-07-12 11:27:47 +0300

    Add error handling without using react query

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -2,30 +2,50 @@ import { useEffect, useState } from "react";
 
 export default function Pokemon() {
   const [pokemon, setPokemon] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     let ignore = false;
-    fetch("http://localhost:4000/pokemon")
-      .then((resp) => resp.json())
+    fetch("http://localhost:4000/pokemon!")
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error("Error with code " + resp.status);
+        }
+        return resp.json();
+      })
       .then((data) => {
         if (!ignore) {
           setPokemon(data);
-          setLoading(false);
+          setIsLoading(false);
+        }
+      })
+      .catch((e) => {
+        if (!ignore) {
+          setError(e.message);
+          setIsLoading(false);
         }
       });
 
     return () => (ignore = true);
   }, []);
 
-  if (loading) {
+  if (isLoading) {
     return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>{error}</h1>
+      </div>
+    );
   }
 
   return (
     <div>
       <h1>Pokemon List (without React Query)</h1>
-      {pokemon.map((p) => (
+      {pokemon?.map((p) => (
         <p key={p.name}>{p.name}</p>
       ))}
     </div>
