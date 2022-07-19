f245a7d09f88013770dd2520181e511e1c7e6473
akifsonmez <akifsonmez@outlook.com>
2022-07-19 22:02:48 +0300

    Complete infinite query example

--- src/pages/InfiniteQueries.js
+++ src/pages/InfiniteQueries.js
@@ -1,8 +1,9 @@
 import { useInfiniteQuery } from "react-query";
+import { Link } from "react-router-dom";
 
 const fetchPokemonList = ({ pageParam = 1 }) => {
   return fetch(
-    `https://pokeapi.co/api/v2/pokemon?offset=${(pageParam - 1) * 20}&limit=20`
+    `https://pokeapi.co/api/v2/pokemon?offset=${(pageParam - 1) * 2}&limit=2`
   ).then((resp) => {
     if (!resp.ok) {
       throw new Error("Error with code " + resp.status);
@@ -12,16 +13,9 @@ const fetchPokemonList = ({ pageParam = 1 }) => {
 };
 
 export default function InfiniteQueries() {
-  const {
-    fetchNextPage,
-    fetchPreviousPage,
-    hasNextPage,
-    hasPreviousPage,
-    isFetchingNextPage,
-    isFetchingPreviousPage,
-  } = useInfiniteQuery(
+  const { fetchNextPage, fetchPreviousPage, data } = useInfiniteQuery(
     "pokemon-infinite",
-    ({ pageParam = 1 }) => fetchPokemonList(pageParam),
+    fetchPokemonList,
     {
       getNextPageParam: (lastPage, allPages) => {
         return allPages.length + 1;
@@ -32,11 +26,23 @@ export default function InfiniteQueries() {
     }
   );
 
+  console.log(data);
   return (
     <div>
       <h1>Pokemon List (with React Query Infinite Queries) </h1>
       <button onClick={fetchPreviousPage}>Prev</button>
       <button onClick={fetchNextPage}>Next</button>
+      {data?.pages?.map((page, i) => (
+        <div key={i}>
+          {page?.results?.map((p) => (
+            <div className="pokemon-list">
+              <Link to={`/rqpokemon/${p.name}`}>
+                <h3>{p.name.toUpperCase()}</h3>
+              </Link>
+            </div>
+          ))}
+        </div>
+      ))}
       {/* {data?.results?.map((p) => (
         <div key={p.name} className="pokemon-list">
           <Link to={`/rqpokemon/${p.name}`}>
